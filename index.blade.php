a6e9434bf89e745649eacba32749d1e63f3844b1
Chiu-Chiu <a925d37@gmail.com>
2019-01-19 16:03:46 +0800

    Create index.blade.php

---
+++ index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TXT_EXPORT</title>
+</head>
+<body>
+<input type="button" name="ex_txt" value="匯出TXT" class="btn btn-info" onclick="javascript:location.href='/export_txt?{{toUrl($params)}}'">
+</body>
+</html>
